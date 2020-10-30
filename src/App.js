131a4ae5a7c2b9244c6e0b87d83c70f1f784f617
Jon Crowell <jcrowell@hksinc.com>
2020-10-30 15:13:24 -0500

    Working example of draggable modal with configurable properties

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "antd/dist/antd.css";
+import "./index.css";
+import { Modal } from "antd";
+import Draggable from "react-draggable";
 
-function App() {
+const App = () => {
+  const [visible, setVisible] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+
+  const showModal = () => setVisible(true);
+
+  const handleOk = (e) => {
+    console.log(e);
+    setVisible(false);
+  };
+
+  const handleCancel = (e) => {
+    console.log(e);
+    setVisible(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <button onClick={showModal}>Open Modal</button>
+      <Modal
+        title={
+          <div
+            style={{
+              width: "100%",
+              cursor: "move",
+            }}
+            onMouseOver={() => {
+              if (disabled) {
+                setDisabled(false);
+              }
+            }}
+            onMouseOut={() => {
+              setDisabled(true);
+            }}
+            // fix eslintjsx-a11y/mouse-events-have-key-events
+            // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/mouse-events-have-key-events.md
+            onFocus={() => {}}
+            onBlur={() => {}}
+            // end
+          >
+            :: Draggable Modal
+          </div>
+        }
+        maskStyle={{
+          // styles for the mask
+          opacity: "20%",
+        }}
+        bodyStyle={{
+          /// styles for the modal body
+          height: 800,
+        }}
+        closable={false} // allow closing of modal with x in title
+        mask={true} // show a mask behind the modal
+        maskClosable={false} // allow user to dismiss modal by clicking outside
+        visible={visible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        modalRender={(modal) => (
+          <Draggable disabled={disabled}>{modal}</Draggable>
+        )}
+      >
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bacon ipsum dolor amet strip steak ball tip tail ham, capicola flank
+          shankle corned beef pig. Frankfurter turkey meatloaf, prosciutto
+          pastrami sausage shank strip steak bresaola tail capicola shankle pork
+          loin shoulder. Short ribs strip steak biltong ball tip, shankle
+          buffalo burgdoggen chislic. Kielbasa beef ribeye, sirloin sausage
+          leberkas alcatra andouille picanha tri-tip strip steak turducken
+          swine. Corned beef ham hock strip steak, pork chop ribeye landjaeger
+          boudin alcatra tenderloin salami chuck. Cow shoulder drumstick, ham
+          hock brisket ground round porchetta spare ribs shankle turkey pork
+          belly hamburger chicken pork chuck.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <p>
+          Porchetta jowl bresaola chicken ground round. Sirloin buffalo leberkas
+          chicken corned beef. Filet mignon shankle pork belly biltong sausage
+          fatback short loin corned beef shoulder turkey alcatra. Ham hock
+          venison chislic leberkas corned beef prosciutto sirloin filet mignon.
+          Beef ground round tongue landjaeger. Tongue kielbasa drumstick,
+          landjaeger brisket beef ham doner.
+        </p>
+        <p>
+          Corned beef sausage spare ribs ham pork belly. Picanha corned beef
+          turkey, meatball brisket tenderloin kevin. Salami shoulder corned beef
+          andouille cupim. Chislic swine ham hock ball tip jowl kevin tongue
+          pork bacon ground round. Andouille prosciutto corned beef, ribeye ham
+          hock boudin frankfurter flank. Leberkas pork chop fatback, t-bone
+          jerky ball tip kielbasa beef burgdoggen. Buffalo venison cupim bacon,
+          turkey meatloaf prosciutto shankle fatback leberkas chuck brisket.
+        </p>
+      </Modal>
+    </>
   );
-}
+};
 
 export default App;
